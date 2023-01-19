c37ac1a6346f0bfb9bb8be1da94e8e2aaf8faa97
Dmitry Boldyr <sunr1sen.dev@gmail.com>
2023-01-20 01:26:09 +0600

    feat: create custom checkbox component

---
+++ src/components/Checkbox/CheckboxCustom.vue
@@ -0,0 +1,38 @@
+<script>
+import { Field, ErrorMessage } from "vee-validate";
+import s from "./CheckboxCustom.modules.scss";
+
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    value: { type: Boolean, required: true },
+    handler: { type: Function, required: true },
+    validator: Function
+  },
+  setup() {
+    // const checkboxValue = ref(props.value);
+    // const checkboxHandler = () => {
+    //   checkboxValue.value = !checkboxValue.value;
+    //   console.log(checkboxValue);
+    // };
+    return { s };
+  },
+  components: { Field, ErrorMessage }
+};
+</script>
+
+<template>
+  <div :class="s.wrapper">
+    <Field
+      :id="name"
+      @change="handler"
+      :value="value"
+      :name="name"
+      type="checkbox"
+      :rules="validator"
+    />
+    <label :for="name">{{ label }}</label>
+    <ErrorMessage :class="s.error" :name="name" />
+  </div>
+</template>
--- src/components/Form/FormCustom.vue
+++ src/components/Form/FormCustom.vue
@@ -1,25 +1,34 @@
 <script>
 import { Form } from "vee-validate";
-import { inputConfig } from "@/services/formService";
+import { inputConfig, selectConfig } from "@/services/formService";
 import InputCustom from "../Input/InputCustom.vue";
 import SelectCustom from "../Select/SelectCustom.vue";
+import CheckboxCustom from "../Checkbox/CheckboxCustom.vue";
+import { checkboxRequired } from "@/services/validators";
 import s from "./FormCustom.modules.scss";
 
 export default {
   name: "FormCustom",
   data: () => ({
+    checkboxValue: false,
     inputConfig,
+    selectConfig,
     s
   }),
   components: {
     InputCustom,
     Form,
-    SelectCustom
+    SelectCustom,
+    CheckboxCustom
   },
   methods: {
     onSubmit(values) {
       console.log(values);
-    }
+    },
+    checkboxHandler() {
+      this.checkboxValue = !this.checkboxValue;
+    },
+    checkboxRequired
   }
 };
 </script>
@@ -27,26 +36,46 @@ export default {
 <template>
   <Form :class="s.form" @submit="onSubmit">
     <InputCustom
-      :style="inputConfig.style.medium"
       :name="inputConfig.name.cardNumber"
+      :style="inputConfig.style.medium"
     />
     <InputCustom
-      :style="inputConfig.style.medium"
       :name="inputConfig.name.cardholderName"
+      :style="inputConfig.style.medium"
     />
     <div :class="s.dates">
-      <SelectCustom :name="'month'" :placeholder="'MM'" />
       <SelectCustom
-        :name="'year'"
+        :name="selectConfig.name.month"
+        :placeholder="'MM'"
+        :style="selectConfig.style.medium"
+      />
+      <SelectCustom
+        :name="selectConfig.name.year"
         :placeholder="'YY'"
-        :style="'large'"
-        required
+        :style="selectConfig.style.medium"
+        :validator="selectConfig.validator.required"
       />
       <InputCustom
-        :style="inputConfig.style.cvc"
         :name="inputConfig.name.cvc"
+        :style="inputConfig.style.cvc"
       />
     </div>
   </Form>
-  <button :class="s.button" @click="onSubmit">Submit (в консоль)</button>
+  <Form>
+    <CheckboxCustom
+      :name="'acceptTerms'"
+      :label="'Accept Terms & Conditions'"
+      :handler="checkboxHandler"
+      :value="checkboxValue"
+      :validator="checkboxRequired"
+    />
+  </Form>
+
+  <button
+    :disabled="!checkboxValue"
+    :class="[s.button, !checkboxValue && s.disabled]"
+    @click="onSubmit"
+  >
+    Submit (в консоль)
+  </button>
 </template>
